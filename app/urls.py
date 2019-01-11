153bd835679a1ea435e9d774f21829870cce2e24
zyz <zyz@1000phone.com>
2019-01-11 11:45:32 +0800

    项目基本结构(文件创建)

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='index'),
+    url(r'^home/$', views.home, name='home'),   # 首页
+    url(r'^market/$', views.market, name='market'), # 闪购超市
+    url(r'^cart/$', views.cart, name='cart'),   # 购物车
+    url(r'^mine/$', views.mine, name='mine'),   # 我的
 ]
